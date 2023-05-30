ccbbe4a4279778469a9e519378fbeabf6ba6abd2
Manu Puyuelo <puyuelo.m@gmail.com>
2023-05-30 15:44:05 +0200

    mobile first

--- components/Home.js
+++ components/Home.js
@@ -184,7 +184,7 @@ function Home() {
       </div>
       <div className={styles.projectSection}>
         <div className={styles.projectContainerLeft}>
-          <div className={styles.projectText}>
+          <div className={styles.projectTextLeft}>
             <p>Découvrez mes</p>
             <br></br>
             <p style={{ fontWeight: "bold" }}>derniers projets.</p>
@@ -192,6 +192,11 @@ function Home() {
           {leftProjects}
         </div>
         <div className={styles.projectContainerRight}>
+          <div className={styles.projectTextRight}>
+            <p>Découvrez mes</p>
+            <br></br>
+            <p style={{ fontWeight: "bold" }}>derniers projets.</p>
+          </div>
           {topProject}
           {rightProjects}
         </div>
@@ -201,46 +206,3 @@ function Home() {
 }
 
 export default Home;
-
-/* <div className={styles.secondSection}>
-        <div className={styles.container2}>
-          <div
-            className={[styles.projectImage0, styles.projectImage1].join(" ")}
-          >
-            <Player
-              autoplay
-              loop
-              speed={1}
-              src="https://assets2.lottiefiles.com/packages/lf20_sg5agyvh.json"
-              className={styles.projectImage0}
-            ></Player>
-
-            <div
-              className={[
-                styles.projectImage0,
-                styles.projectImage1,
-                styles.projectImage2,
-              ].join(" ")}
-            ></div>
-          </div>
-
-          <div className={styles.projectDescription1}>
-            <h2>Ourson</h2>
-            <p>
-              Ourson est la 1ère app intelligente qui vous donne un coup de
-              patte concernant l’éveil de votre enfant sur 3 aspects :
-              nutrition, suivi du sommeil et activités.
-            </p>
-            <div className={styles.heroButtons}>
-              <div
-                className={[styles.heroButton, styles.heroButton2].join(" ")}
-              >
-                <a href="/contact">
-                  <p>DÉCOUVRIR </p>
-                  <FontAwesomeIcon icon={faChevronRight} />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */
