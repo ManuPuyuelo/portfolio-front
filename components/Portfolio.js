d8376e5b96eaab8d7856586639541442a1414229
Manu Puyuelo <puyuelo.m@gmail.com>
2023-05-30 21:20:11 +0200

    MVP website mid-final

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,9 +1,11 @@
 import styles from "../styles/Portfolio.module.css";
+import ProjectsSection from "../components/ProjectsSection";
 
 function Portfolio() {
   return (
     <main className={styles.main}>
-      <div className={styles.container}>0</div>
+      <div className={styles.container}></div>
+      <ProjectsSection />
     </main>
   );
 }
