b4a9dfb59ae9bce6b28f53b6d22f5c1ac4b6e7fc
Manu Puyuelo <puyuelo.m@gmail.com>
2023-06-10 17:01:37 +0200

    addition meta

--- pages/_app.js
+++ pages/_app.js
@@ -18,8 +18,44 @@ function App({ Component, pageProps }) {
             `,
           }}
         />
-        <title>Manu Puyuelo</title>
+
+        <meta
+          name="description"
+          content="Développeur Fullstack. JS, HTML, CSS React, React Native, Express. Découvrez mes derniers projets et prenez contact avec moi."
+        />
+        <meta
+          name="keywords"
+          content="Développeur Fullstack, JS, HTML, CSS, React, React Native, Express, projets, contact, Manu Puyuelo, La Capsule"
+        />
+        <meta
+          property="og:title"
+          content="Manu Puyuelo - Développeur FullStack"
+        />
+        <meta
+          property="og:description"
+          content="Maîtrisant les technologies telles que JavaScript, HTML, CSS, React, React Native et Express. En reconversion après 7 ans de marketing, je crée des applications web interactives et des expériences utilisateur exceptionnelles. Explorez mes projets et contactez-moi pour discuter de vos idées.  "
+        />
+        <meta property="og:url" content="https://www.manupuyuelo.com" />
+        <meta
+          property="og:image"
+          content="https://www.manupuyuelo.com/_next/static/media/photoManu.78e740fd.jpg"
+        />
+        <title>Manu Puyuelo - Développeur FullStack</title>
         <meta lang="fr" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `
+              {
+                "@context": "https://schema.org",
+                "@type": "ImageObject",
+                "url": "https://www.manupuyuelo.com/_next/static/media/photoManu.78e740fd.jpg",
+                "width": "1129",
+                "height": "1129"
+              }
+            `,
+          }}
+        />
       </Head>
       {/* Code du Google Tag Manager à la balise <body> */}
       <div
